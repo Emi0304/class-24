9d52c9c16f1ec732136ea975725abacd1790f012
Emi0304 <sadiaparvin0304@gmail.com>
2023-03-17 15:45:20 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,13 @@
+var name = "emi";
+console.log(name)
+
+var age = 19;
+
+console.log("My name is", name , 'and My age is ' ,age)
+
+var price = "20 &";
+
+console.log("First price : " ,price)
+
+price ="30 &"
+console.log("Second price : " , price)
